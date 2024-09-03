0199e0f28ac9109588454eb801a15cf5939f74b7
pro <pro@pros-MacBook-Pro.local>
2024-09-03 19:40:12 +0200

    updated

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 // Route to handle contact form submissions
 app.post('/contact', (req, res) => {
