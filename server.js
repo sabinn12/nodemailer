6e38cd4254d800fa4336f96474e0fa65d5644d61
pro <pro@pros-MacBook-Pro.local>
2024-09-03 19:34:01 +0200

    updated

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Enable CORS for all origins (or specify a specific origin)
 app.use(cors({
-    origin: 'https://sabinn.netlify.app/' ,
+    origin: ['https://sabinn.netlify.app/'] ,
     methods: ['POST']// Replace with your frontend's origin
 }));
 
